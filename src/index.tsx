d667f4b35cfa2bf1df5717269bca5dbebe0a4fb8
lihao <hao.li@xbotspace.com>
2023-07-21 14:24:08 +0800

    feat: 🎸 init

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import Didact from './lib/Didact';
+
+const App = (props: { name: string }) => {
+  const [v, setV] = Didact.useState(1);
+  return (
+    <div onClick={() => setV((v: number) => v + 2)}>
+      <h1 title="foo">Hello {props.name}</h1>
+      <div>{v}</div>
+    </div>
+  );
+};
+
+console.log(App);
+
+Didact.render(<App name="1"></App>, document.getElementById('root'));
+// ReactDOM.render(<App name="1"></App>, document.getElementById('root'));
